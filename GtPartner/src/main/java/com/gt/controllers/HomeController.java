41b36adc4b7d834989fc012d8e31cde3765ddd22
aloukikroy07 <aloukik.roy@gigatechltd.com>
2023-02-09 09:30:14 +0600

    s

--- GtPartner/src/main/java/com/gt/controllers/HomeController.java
+++ GtPartner/src/main/java/com/gt/controllers/HomeController.java
@@ -1,7 +1,5 @@
 package com.gt.controllers;
 
-import java.io.Console;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -9,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
