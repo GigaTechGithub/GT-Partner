8d9bc2009bc1ec3cf155eb02b310b451adfa4075
aloukikroy07 <aloukik.roy@gigatechltd.com>
2023-02-14 09:17:46 +0600

    as

---
+++ GtPartner/src/main/java/com/gt/models/FinDoc3.java
@@ -0,0 +1,96 @@
+package com.gt.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="T_fin_doc3")
+public class FinDoc3 {
+	@Id
+ 	@Column(name="ID")
+ 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+     
+    @Column(name="CREATED_BY")
+    private Integer createdBy;
+    
+    @Column(name="DILIGENCE_ID")
+    private Integer diligenceId;
+    
+    @Column(name="FILE_NAME")
+    private String fileName;
+    
+    @Column(name="FILE_PATH")
+    private String filePath;
+    
+    @Column(name="FILE_TYPE")
+    private String fileType;
+
+	public FinDoc3() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public FinDoc3(Long id, Integer createdBy, Integer diligenceId, String fileName, String filePath, String fileType) {
+		super();
+		this.id = id;
+		this.createdBy = createdBy;
+		this.diligenceId = diligenceId;
+		this.fileName = fileName;
+		this.filePath = filePath;
+		this.fileType = fileType;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(Integer createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Integer getDiligenceId() {
+		return diligenceId;
+	}
+
+	public void setDiligenceId(Integer diligenceId) {
+		this.diligenceId = diligenceId;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
+	public String getFileType() {
+		return fileType;
+	}
+
+	public void setFileType(String fileType) {
+		this.fileType = fileType;
+	}
+    
+}
---
+++ GtPartner/src/main/java/com/gt/models/FinDoc4.java
@@ -0,0 +1,97 @@
+package com.gt.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="T_fin_doc4")
+public class FinDoc4 {
+	@Id
+ 	@Column(name="ID")
+ 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+     
+    @Column(name="CREATED_BY")
+    private Integer createdBy;
+    
+    @Column(name="DILIGENCE_ID")
+    private Integer diligenceId;
+    
+    @Column(name="FILE_NAME")
+    private String fileName;
+    
+    @Column(name="FILE_PATH")
+    private String filePath;
+    
+    @Column(name="FILE_TYPE")
+    private String fileType;
+
+	public FinDoc4() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public FinDoc4(Long id, Integer createdBy, Integer diligenceId, String fileName, String filePath, String fileType) {
+		super();
+		this.id = id;
+		this.createdBy = createdBy;
+		this.diligenceId = diligenceId;
+		this.fileName = fileName;
+		this.filePath = filePath;
+		this.fileType = fileType;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(Integer createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Integer getDiligenceId() {
+		return diligenceId;
+	}
+
+	public void setDiligenceId(Integer diligenceId) {
+		this.diligenceId = diligenceId;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
+	public String getFileType() {
+		return fileType;
+	}
+
+	public void setFileType(String fileType) {
+		this.fileType = fileType;
+	}
+    
+    
+}
