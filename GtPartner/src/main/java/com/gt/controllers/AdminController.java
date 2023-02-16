fdd75cd3326a9ca5f2b7abc4938a0ce3431e2134
aloukikroy07 <aloukik.roy@gigatechltd.com>
2023-02-16 16:21:09 +0600

    saf

--- GtPartner/src/main/java/com/gt/controllers/AdminController.java
+++ GtPartner/src/main/java/com/gt/controllers/AdminController.java
@@ -1,13 +1,31 @@
 package com.gt.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.gt.models.Diligence;
+import com.gt.services.DiligenceService;
 
 @Controller
 public class AdminController {
+	@Autowired
+	DiligenceService diligenceService;
 	
 	@GetMapping("/admin")
     public String home() {
         return "admin-view/home/admin-home";
+    }
+	
+	@GetMapping("/partners")
+    public String partnerList(Model model) {
+		List<Diligence> diligenceList = diligenceService.findBystatus(1);
+		
+		model.addAttribute("diligenceList", diligenceList);
+		
+        return "admin-view/partners/partnerList";
     }
 }
