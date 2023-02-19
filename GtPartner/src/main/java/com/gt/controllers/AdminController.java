e45e9987f61cdd1acb97d14c3240a60f232bcaf4
aloukikroy07 <aloukik.roy@gigatechltd.com>
2023-02-19 10:59:41 +0600

    s

--- GtPartner/src/main/java/com/gt/controllers/AdminController.java
+++ GtPartner/src/main/java/com/gt/controllers/AdminController.java
@@ -14,14 +14,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.gt.models.AjaxResponse;
 import com.gt.models.Bod;
